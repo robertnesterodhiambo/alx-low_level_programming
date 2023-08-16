a799808e1c140ab212288beab3898394e8a704b2
Idriss Talbi <i.talbi@student.uw.edu.pl>
2023-08-16 02:41:27 -0400

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,18 @@
+julien@ubuntu:~/0x0c. macro, structures$ cat 0-main.c
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int s;
+
+    s = 98 + SIZE;
+    printf("%d\n", s);
+    return (0);
+}
